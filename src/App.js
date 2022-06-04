412db5b69c6bb8c96535134b94c51b220eca6aac
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2022-06-04 22:40:49 +0530

    Work on Navbar Started.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>Hello</div>
-  )
-};
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
