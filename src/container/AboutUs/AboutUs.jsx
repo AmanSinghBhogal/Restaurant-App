4a1407a0b6e1dc3b2cabc5e3ca37f09f3de9ad55
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2022-06-25 17:54:28 +0530

    Added About Us Section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">
+          About Us
+        </h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img'/>
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eveniet officia voluptatem fuga, vero eius eligendi deserunt placeat saepe commodi delectus odio molestias sed enim cum. Veniam qui earum architecto?</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="Knife" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">
+          Our History
+        </h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img'/>
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa velit tempore odio itaque provident atque expedita deleniti voluptatum voluptas! Quae quam vitae molestias obcaecati.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+    </div>
   </div>
 );
 
