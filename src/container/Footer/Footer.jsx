4e8db22949d33b0d92aa1e0efef290c24139eb7a
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2022-07-06 17:22:18 +0530

    Work Going on Footer.js

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
 
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        
+      </div>
+      <div className="app__footer-links_logo">
+
+      </div>
+      <div className="app__footer-links_work">
+
+      </div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">
+      {new Date().getFullYear()} Gericht. All Rights Reserved.
+      </p>
+    </div>
   </div>
 );
 
